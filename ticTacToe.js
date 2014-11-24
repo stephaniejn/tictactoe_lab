353451f2ea072c0e3e3f242c5ba742550d0957f0
stephaniejn <stephaniejnilles@gmail.com>
2014-11-24 12:11:43 -0800

    added win, tie and counter functions

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,7 +1,15 @@
 
 // Load Page
 document.addEventListener("DOMContentLoaded", function(){
-	alert("Player One: You're Up!");
+	alert("Player X: You're Up!");
+
+
+// Variables
+var winningCombo = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
+var board = document.getElementsByClassName('square');
+console.log(board);
+
+
 
 // Display X or O in each square
 var col1 = document.getElementById("col1").addEventListener("click", click)
@@ -14,15 +22,43 @@ var col7 = document.getElementById("col7").addEventListener("click", click)
 var col8 = document.getElementById("col8").addEventListener("click", click)
 var col9 = document.getElementById("col9").addEventListener("click", click)
 
+// Check for Winner
+var checkForWinner = function (){
 
+	console.log("checking..");
+	// for loop with winning combos
+	// check each winning combo against board[]
+	for (var i = 0; i< winningCombo.length; i++) {
+		if(board[winningCombo[i][0]].innerText !== "" &&
+			board[winningCombo[i][0]].innerText === board[winningCombo[i][1]].innerText &&
+			board[winningCombo[i][1]].innerText === board[winningCombo[i][2]].innerText
+		){
+			alert("Congrats Player " + board[winningCombo[i][2]].innerText + "!"); 
+		reset()
+			}
+			
+	}
+
+}
+
+// Check for Tie
+var checkForTie = function(){
+	if(numMoves === 9 
+			)
+			{alert("You tied, try again!")
+		reset()};
+}
 
 // Determine whose turn it is
 var nextTurn= "X";
+var numMoves= 0;
 
 function click(){
 	if(document.getElementById(this.id).innerHTML === ""){
 		document.getElementById(this.id).innerHTML = nextTurn;
+		checkForWinner();
 		switchTurn();
+		checkForTie();
 	}
 	else{
 		alert("I'm already selected...Try again!");
@@ -30,16 +66,27 @@ function click(){
 }
 
 // Switch turns
+var counterx=0
+var countery=0
+
+
+//Switch turn and increase counter
 function switchTurn()
 {
 	if(nextTurn === "X")
 	{	 
 nextTurn = "O";
-alert("It's Player Two's Turn!");
+counterx ++;
+numMoves++;
+document.getElementById("counterx").innerHTML  = counterx; 
+// alert("It's Player O's Turn!");
 	}
 	else {
 		nextTurn = "X";
-alert("It's Player One's Turn!");
+countery ++;
+numMoves++;
+document.getElementById("countery").innerHTML  = countery; 
+// alert("It's Player X's Turn!");
 	}
 } 
 
@@ -52,35 +99,3 @@ document.getElementById("reset").addEventListener("click", function() {
 });
 
 })
-
-// Couldn't get the win function - here is my work
-
-//Check if a player won
-// board: 1 2 3
-// 		  4 5 6
-// 		  7 8 9
-// Winning combos:
-// col1: (col1, col2, col3) (col1, col5, col9), (col1, col4, col7)
-// col2: (col2, col5, col8)
-// col3: (col3, col6, col9) (col3, col5, col7)
-// col4: (col4, col5, col6) 
-// col7: (col7, col8, col9)
-
-
-// 	}
-// } checkWin()
-
-// function checkWin(){
-// 	if (col2 === "X" && col5 === "X" && col8 === "X")
-// 	{
-// 		return 1
-// 	}
-// 	}
-// etc...
-// or
-
-// switch(check){
-// 	case col1 === "X" && col2 === "X" && col3 === "X":
-// 		alert("Player One Won!")
-// 		break;
-// // }
